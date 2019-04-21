f9131e6effadb6a07f8bb3d38592c1541cef6bb0
gyonmyon <gyonmyon@gmai.com>
2019-04-21 18:45:39 +0300

    comment

--- timer.py
+++ timer.py
@@ -1,6 +1,6 @@
 import time
 
-#determine the Meeting date
+# determine the Meeting date
 
 print("Input meeting date in formate DD.MM.YYYY HH:MM")
 time_string = input()
@@ -10,19 +10,19 @@ result = time.mktime(t)
 
 print("Meeting date:", time_string)
 
-#determine the local date
+# determine the local date
 named_tuple = time.localtime()
 nowPrint = time.strftime("%d %B, %Y %H:%M", named_tuple)
 
 print("Your local time:", nowPrint)
 
-#Count
+# Count
 localNum = int(round(time.time()))
 
 seconds_remain = int(result)
 countback = abs(localNum - seconds_remain)
-    
-#Captain Obvious
+
+# Captain Obvious
 second = 1
 minute = 60 * second
 hour = 60 * minute
@@ -32,18 +32,18 @@ print("\n")
 days = countback // day
 print("Whole days to the meeting:", days)
 
-hours = countback  // hour
+hours = countback // hour
 print("Whole hours to the meeting:", hours)
 
-minutes = countback  // minute
+minutes = countback // minute
 print("Whole minutes to the meeting:", minutes)
 
 seconds = countback // second
 print("Whole seconds to the meeting:", seconds)
 
-###Time to meeting
-hrsTo = (countback % day) //hour
-minTo = (countback % hour) //minute
+# Time to meeting
+hrsTo = (countback % day) // hour
+minTo = (countback % hour) // minute
 secTo = (countback % minute)
-print("It remains quite a bit:", days,"days,", hrsTo, "hours,", minTo, "minutes,", secTo, "seconds.")
-
+print("It remains quite a bit:", days, "days,", hrsTo,
+      "hours,", minTo, "minutes,", secTo, "seconds.")
